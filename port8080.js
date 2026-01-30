77c4fa14fa4392a589ae8345c5e4621644288db8
Antimony1002 <18293153985@163.com>
2026-01-30 21:49:09 +0800

    Create a basic HTTP server in port8080.js

---
+++ port8080.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  let filePath = './jiaowotong_interactive.html';
+  if (req.url !== '/' && req.url !== '/jiaowotong_interactive.html') {
+    filePath = '.' + req.url;
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'application/image/svg+xml'
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile('./jiaowotong_interactive.html', (err, fallbackContent) => {
+          if (err) {
+            res.writeHead(500);
+            res.end('500 - Internal Server Error');
+          } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(fallbackContent, 'utf-8');
+          }
+        });
+      } else {
+        res.writeHead(500);
+        res.end('500 - Internal Server Error');
+      }
+    } else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
